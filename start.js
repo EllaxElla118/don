323bbf06662bdc0c256b08f74b9fd9eb35c74c3f
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-10-31 11:26:01 +0100

    Update start.js

--- start.js
+++ start.js
@@ -11,15 +11,17 @@ const puppeteer = require('puppeteer');
   await page.setViewport({ width: 1280, height: 800 });
   
   // Navigate to the desired URL
-  await page.goto('https://meta.ai/');
+  await page.goto('https://meta.ai/', { waitUntil: 'networkidle0' });
 
   // Select the input field with the specified placeholder and type "hello"
   await page.waitForSelector('textarea[placeholder="Ask Meta AI anything..."]');
   await page.type('textarea[placeholder="Ask Meta AI anything..."]', 'hello');
 
   // Select the div with the attribute label="Send message" and click it
-  await page.waitForSelector('path[d="M13 7.414V19a1 1 0 1 1-2 0V7.414l-3.293 3.293a1 1 0 0 1-1.414-1.414l5-5a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1-1.414 1.414L13 7.414z"]');
-  await page.click('path[d="M13 7.414V19a1 1 0 1 1-2 0V7.414l-3.293 3.293a1 1 0 0 1-1.414-1.414l5-5a1 1 0 0 1 1.414 0l5 5a1 1 0 0 1-1.414 1.414L13 7.414z"]');
+  const pathXPath = "//path[contains(@d, 'M13 7.414')]";
+  await page.waitForXPath(pathXPath);
+  const [pathElement] = await page.$x(pathXPath);
+  await pathElement.click();
     
  await page.evaluate(() => {
     const span = Array.from(document.querySelectorAll('span')).find(el => el.textContent === 'Continue without logging in');
@@ -28,8 +30,6 @@ const puppeteer = require('puppeteer');
     }
   });
   
-  await page.waitForSelector('path[d="M4.341 7.247a1 1 0 0 0-.094 1.412l7 8a1 1 0 0 0 1.506 0l7-8a1 1 0 0 0-1.506-1.318L12 14.482l-6.247-7.14a1 1 0 0 0-1.412-.094z"]');
-
   // Take a screenshot and save it as 'screenshot.png'
   let t = await page.screenshot({ encoding: 'base64', fullPage: true });
   console.log(t);
